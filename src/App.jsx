6481354851b7448b2d76e736064b5ab0df68a4e8
Jesse Krensel <jdkrensel@gmail.com>
2023-03-02 00:45:20 -0500

    add state and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,84 @@
 import React from "react";
 
-import {
-    CssBaseline,
-    Box,
-    Container,
-    Typography,
-    Stack,
-    Link,
-} from "@mui/material";
+import { CssBaseline, Container, Stack } from "@mui/material";
 
 import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
-import Definition from "./components/Definition";
+import Definitions from "./components/Definitions";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { useState } from "react";
 
-const darkTheme = createTheme({
-    palette: {
-        mode: "dark",
-        secondary: {
-            main: "#a445ed",
-        },
-    },
-});
-
-const lightTheme = createTheme({
-    palette: {
-        mode: "light",
-        secondary: {
-            main: "#a445ed",
-        },
-    },
-
-});
-
 function App() {
-    const [theme, setTheme] = useState(lightTheme);
-    const [searchTerm, setSearchTerm] = useState("engineering")
-    console.log(theme)
+    const [mode, setMode] = useState("light");
+    const [font, setFont] = useState("Inter")
+    const [searchTerm, setSearchTerm] = useState("play");
+
+    const theme = createTheme({
+        palette: {
+            mode: mode,
+            secondary: {
+                main: "#a445ed",
+            },
+        },
+        typography: {
+            fontFamily: font,
+            h1: {
+                //word
+                fontSize: 64,
+                fontWeight: "bold",
+                marginTop: 39,
+                marginBottom: 10,
+            },
+            h2: {
+                //partOfSpeech
+                fontSize: 24,
+                fontWeight: "bold",
+                fontStyle: "italic",
+                marginTop: 24,
+            },
+            h3: {
+                //meaning/synonyms
+                fontSize: 20,
+                marginTop: 40,
+                marginBottom: 25,
+                color: "#757575",
+            },
+            h4: {
+                //input / synonym list
+                fontSize: 20,
+                fontWeight: "bold",
+            },
+            body1: {
+                //definition list items
+                fontSize: 18,
+                lineHeight: 1.33,
+            },
+            body2: {
+                //source
+                fontSize: 14,
+                color: "#757575",
+            },
+        },
+    });
+    console.log(`Your theme is: \n`, theme);
     const toggleTheme = () => {
-        theme === lightTheme ? setTheme(darkTheme) : setTheme(lightTheme);
+        mode === "light" ? setMode("dark") : setMode("light");
     };
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline>
-                <NavBar theme={theme} darkTheme={darkTheme} onToggle={toggleTheme}/>
+                <NavBar theme={theme} onToggle={toggleTheme} mode={mode} onFont={setFont} />
                 <Container component="main">
                     <Stack spacing={2}>
-                        <SearchBar theme={theme} onSearch={setSearchTerm}/>
-                        <Definition searchTerm={searchTerm}/>
-                        <Box sx={{ pb: "2rem" }}>
-                            <Typography variant="h4">Source</Typography>
-                            <Link>source.com</Link>
-                        </Box>
+                        <SearchBar theme={theme} onSearch={setSearchTerm} searchTerm={searchTerm} />
+
+                        <Definitions
+                            searchTerm={searchTerm}
+                            mode={mode}
+                            onClick={setSearchTerm}
+                            theme={theme}
+                        />
                     </Stack>
                 </Container>
             </CssBaseline>
